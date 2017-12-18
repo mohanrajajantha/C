9876734ba6cc1ddb2c390136a3ffadd491697c1e
MOHANRAJ D <15euee081@skcet.ac.in>
2017-12-18 10:31:47 +0000

    Create asg5.c

---
+++ beginner/Assignment/asg5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+int a=10,b=5,add,sub,mul,div,mod;
+
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+printf("%d\n%d\n%d\n%d\n",add,sub,mul,div,mod);
+return 0;
+}
