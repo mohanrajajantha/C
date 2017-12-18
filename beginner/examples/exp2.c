2fafd10cd4a0a29db3570d0444e49a174b38a060
MOHANRAJ D <15euee081@skcet.ac.in>
2017-12-18 07:28:16 +0000

    Create exp2.c

---
+++ beginner/examples/exp2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main(void) {
+	 int a, b;
+ 
+   int c;
+ 
+   float f;
+ 
+   a = 10;
+ 
+   b = 20;
+ 
+   c = a + b;
+ 
+   printf("value of c : %d ", c);
+ 
+   f = 70.0/3.0;
+ 
+   printf("value of f : %f ", f);
+ 
+ 
+	return 0;
+}
