9878bf097061daae7fea749b03d32981123f30e4
MOHANRAJ D <15euee081@skcet.ac.in>
2017-12-18 08:43:31 +0000

    Create ass2.c

---
+++ beginner/Assignment/ass2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+float avg=44.83;
+int run=18426,match=463;
+char name[100]={"Sachin Ramesh Tendulkar"},nickname[100]={"Little master"};
+printf(" Name:%s\n Nick name:%s\n Runs scored in ODI:%d\n Batting average:%d\n ODI matches:%f\n",name,nickname,run,avg,run);
+return 0;
+}
