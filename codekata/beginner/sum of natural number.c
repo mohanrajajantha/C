a343c112f10b2bb64c8992ee30f69a6542e0d6ab
MOHANRAJ D <15euee081@skcet.ac.in>
2018-01-14 15:07:13 +0530

    Update sum of natural number.c

--- codekata/beginner/sum of natural number.c
+++ codekata/beginner/sum of natural number.c
@@ -4,10 +4,14 @@ int main (void)
 int i=0,n,s=0;
 printf("Enter the value:");
 scanf("%d",&n);
+  if(n>0){
 sum:
 s=s+i;
 i++;
 if(i<=5)
 goto sum;
 printf("%d",s);
+  }
+  else
+    printf("invalid")
 }
