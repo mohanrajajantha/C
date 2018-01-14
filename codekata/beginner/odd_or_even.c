7a2ab1aaf1ffea902edec7067b1b4ad2ab5d6f7a
MOHANRAJ D <15euee081@skcet.ac.in>
2018-01-14 14:56:20 +0530

    Update odd_or_even.c

--- codekata/beginner/odd_or_even.c
+++ codekata/beginner/odd_or_even.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<ctype.h>
 int main (void)
 {
 int n;
 printf("Enter the value:");
 scanf("%d",&n);
+  if(isdigit(n))
+  {
   if(n>=1&&n<=10000)
   {
 if(n%2==0)
@@ -11,7 +14,7 @@ printf(" Even");
 else
 printf("ODD");
   }
-  else
+  }  else
     printf("Invalid");
   return 0;
 }
