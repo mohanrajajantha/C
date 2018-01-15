5c0b2f667ed61a96e144646d2cb7e83ed32af0c0
MOHANRAJ D <15euee081@skcet.ac.in>
2018-01-15 14:13:05 +0530

    Create evennumber.c

---
+++ codekata/beginner/evennumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int n,m,i;
+   scanf("%d%d",&n,&m);
+   for(i=n+1;i<m;i++)
+   {
+       if(i%2=0)
+       printf("%d ",i);
+   }
+    return 0;
+}
