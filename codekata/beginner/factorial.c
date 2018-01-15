c74adcdf688804e6e86bd5f0f1c6ea488bb15fb3
MOHANRAJ D <15euee081@skcet.ac.in>
2018-01-15 15:13:28 +0530

    Create factorial.c

---
+++ codekata/beginner/factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int n,s=1;
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++)
+   {
+       s=s*i;
+   }
+printf("%d",s);
+   
+    return 0;
+}
+
+
+
