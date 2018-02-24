966e466b322cd3949f928fcfa68081933eea70a1
MOHANRAJ D <15euee081@skcet.ac.in>
2018-02-24 21:40:05 +0530

    Create sort.c

---
+++ codekata/beginner/sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a[100000],n,min,i,j,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+
+for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+	{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+	}
+}
+printf("Sorted array\n");
+for(i=0;i<n;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+
+}
