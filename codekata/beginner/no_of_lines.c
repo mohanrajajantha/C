9ca9491b52b5e142efe0865f6e2b6f52b5fbe897
MOHANRAJ D <15euee081@skcet.ac.in>
2018-03-07 21:08:18 +0530

    Create no_of_lines.c

---
+++ codekata/beginner/no_of_lines.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+	char s[100000];
+	int i,count=0;
+	gets(s);
+	int k=strlen(s);
+	for(i=0;i<k;i++)
+	{
+		if(s[i]=='\n')
+		count++;
+		
+	}
+	printf("%d",count+1);
+}
