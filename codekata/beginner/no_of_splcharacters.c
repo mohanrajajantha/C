e06d669c8b8cbf0df404861f87a6261844e164b7
MOHANRAJ D <15euee081@skcet.ac.in>
2018-03-07 21:10:35 +0530

    Create no_of_splcharacters.c

---
+++ codekata/beginner/no_of_splcharacters.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+	char s[100000];
+	int i,count=0,spl;
+	gets(s);
+	int k=strlen(s);
+	for(i=0;i<k;i++)
+	{
+		if(isdigit(s[i]))
+		count++;
+		if(isalpha(s[i]))
+        count++		;
+	}
+	spl=k-count;
+	printf("%d",spl);
+}
