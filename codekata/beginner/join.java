c7c9c24d275d7279eea26ef6e597532b73542477
MOHANRAJ D <mohanrajajantha@gmail.com>
2018-03-08 09:42:39 +0530

    Create join.java

---
+++ codekata/beginner/join.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class join
+{
+	public static void main (String[] args) 
+	{
+	Scanner input=new Scanner(System.in);
+	String k=input.nextLine();
+	System.out.println(k+'.');
+	}
+}
+	
