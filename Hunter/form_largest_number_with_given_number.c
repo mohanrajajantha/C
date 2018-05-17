f29291a575bdebffef657ab948117752b2ea38b3
MOHANRAJ D <mohanrajajantha@gmail.com>
2018-05-17 21:23:14 +0530

    Create form_largest_number_with_given_number.c

---
+++ Hunter/form_largest_number_with_given_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int x,a[100],i,j,temp;
+	scanf("%d",&x);
+	for(i=0;i<x;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<x;i++)
+	{
+		for(j=i+1;j<x;j++)
+		{
+			if(a[i]<a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<x;i++)
+	printf("%d",a[i]);
+	return 0;
+}
