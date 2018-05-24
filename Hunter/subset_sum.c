7f01bca5da341d6608e6f9dee8fdd1f511fd2e0f
MOHANRAJ D <mohanrajajantha@gmail.com>
2018-05-24 22:41:20 +0530

    Create subset_sum.c

---
+++ Hunter/subset_sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],n,i,j,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	if(a[i]>=0)
+	{
+		sum=sum+a[i];
+	}
+	}
+	printf("%d",sum);
+	return 0;
+	
+}
